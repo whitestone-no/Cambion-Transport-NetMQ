c8694c67eaeae7bc44745d7cd9ac2626b078d002
Gina Therese Hvidsten <8248605+gthvidsten@users.noreply.github.com>
2025-03-26 14:48:40 +0100

    Added methods to read from configuration

--- src/NetMqTransportExtensions.cs
+++ src/NetMqTransportExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Whitestone.Cambion.Interfaces;
@@ -7,10 +8,31 @@ namespace Whitestone.Cambion.Transport.NetMQ
 {
     public static class NetMqTransportExtensions
     {
-        // ReSharper disable once InconsistentNaming
         public static ICambionSerializerBuilder UseNetMqTransport(this ICambionTransportBuilder builder, Action<NetMqConfig> configure)
         {
+            return UseNetMqTransport(builder, null, configure, null);
+        }
+
+        public static ICambionSerializerBuilder UseNetMqTransport(this ICambionTransportBuilder builder, IConfiguration configuration, string cambionConfigurationKey = "Cambion")
+        {
+            return UseNetMqTransport(builder, configuration, null, cambionConfigurationKey);
+        }
+
+        public static ICambionSerializerBuilder UseNetMqTransport(this ICambionTransportBuilder builder, IConfiguration configuration, Action<NetMqConfig> configure, string cambionConfigurationKey = "Cambion")
+        {
             builder.Services.Replace(new ServiceDescriptor(typeof(ITransport), typeof(NetMqTransport), ServiceLifetime.Singleton));
+
+            if (configuration != null)
+            {
+                string assemblyName = typeof(NetMqTransport).Assembly.GetName().Name;
+
+                IConfigurationSection config = configuration.GetSection(cambionConfigurationKey).GetSection("Transport").GetSection(assemblyName);
+
+                if (config.Exists())
+                {
+                    builder.Services.Configure<NetMqConfig>(config);
+                }
+            }
 
             builder.Services.AddOptions<NetMqConfig>()
                 .Configure(conf => { configure?.Invoke(conf); });
